b18c9bdab0013b5dc96f5cd7ea1077ac015a9827
NarmadaGunasekaran <gnarmada77@gmail.com>
2026-03-30 15:09:17 +0200

    Selenium my first commit

--- src/test/java/com/test1/FirstClass.java
+++ src/test/java/com/test1/FirstClass.java
@@ -1,4 +1,17 @@
 package com.test1;
 
+import org.testng.annotations.Test;
+
 public class FirstClass {
+
+    @Test
+    public void firstMethod(){
+        System.out.println("Hello World");
+    }
+    @Test
+    public void firstMethod1(){
+        System.out.println("Hello World2");
+    }
+
+
 }
