50bfcc94fd022c7b812f6fe4c677f8b1ab5af410
NarmadaGunasekaran <gnarmada77@gmail.com>
2026-04-05 16:22:43 +0200

    Selenium Get API Code

--- src/main/java/org/example/BaseClass.java
+++ src/main/java/org/example/BaseClass.java
@@ -2,7 +2,7 @@ package org.example;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class BaseClass {
     static void main() {
         System.out.println("Hello World");
         }
